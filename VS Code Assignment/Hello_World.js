ea655fa72927157749b3341391b1fdf9abb0234c
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-05-23 16:21:45 -0700

    Adding
    Adding new files

---
+++ VS Code Assignment/Hello_World.js
@@ -0,0 +1,8 @@
+var greeting = 'Hello World!';
+console.log(greeting);
+
+var greeting2 = 'Welcome to Node.js';
+console.log(greeting2);
+
+car sentence1 = 'Learning how to debug code with the debugger';
+console.log(sentenceOnce);
